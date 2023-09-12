a9600a7c7ef1d1d2d191c062e30dda01a8bc7750
Gonzalomorales1001 <gonzalomorales1001@gmail.com>
2023-09-12 11:29:26 -0300

    Projects section layout finished

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,8 +13,8 @@ const Contact = () => {
                         <p className="fw-light"><i className="fa fa-envelope-o text-aqua me-2" aria-hidden="true"></i>gonzalomorales1001@gmail.com</p>
                         <p className="fw-light"><i className="fa fa-map-marker text-aqua me-2" aria-hidden="true"></i>Tucumán - Argentina</p>
                         <h5 className="text-aqua text-center mt-4 d-none d-md-block">&lt;<span className="text-light">Más Información</span>&nbsp; &frasl; &gt;</h5>
-                        <a href="https://www.linkedin.com/in/gonzalomorales-web" className="d-block button w-100 mt-2"><i class="fa fa-linkedin-square me-3" aria-hidden="true"></i>LinkedIn</a>
-                        <a href="https://github.com/Gonzalomorales1001" className="d-block button w-100 mt-2"><i class="fa fa-github me-3" aria-hidden="true"></i>GitHub</a>
+                        <a href="https://www.linkedin.com/in/gonzalomorales-web" className="d-block button w-100 mt-2"><i className="fa fa-linkedin-square me-3" aria-hidden="true"></i>LinkedIn</a>
+                        <a href="https://github.com/Gonzalomorales1001" className="d-block button w-100 mt-2"><i className="fa fa-github me-3" aria-hidden="true"></i>GitHub</a>
                     </div>
                     <div className="col">
                         <div className="aqua-border">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,67 @@
 import React, { useState, useContext } from 'react';
 import { DarkModeContext } from '../App';
+import folder from '../assets/svg/folder.svg';
+import '../css/Projects.css'
 
 const Projects = () => {
     const { dark } = useContext(DarkModeContext);
     return (
-        <section className="vh-100" id='Projects'>
-            <div className="container flex-center h-100">
-
+        <section className={`vh-100 ${dark && 'text-light'}`} id='Projects'>
+            <div className="container flex-column d-flex justify-content-center gap-2 h-100">
+                <div className="row">
+                    <div className="col">
+                        <div className="aqua-border">
+                            <div className="box my-4 d-flex">
+                                <div className="folder">
+                                    <img src={folder} alt="folder" />
+                                </div>
+                                <div className='ps-3'>
+                                    <h4 className="text-aqua text-uppercase mt-4">&lt;<span className="text-light">Prácticas</span>&nbsp; &frasl; &gt;</h4>
+                                    <p className="fw-200">
+                                        Aquí podras ver las pequeñas practicas y retos que he realizado para afianzar mis conocimientos
+                                    </p>
+                                    <button className="button">Abrir</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="aqua-border">
+                            <div className="box my-4 d-flex">
+                                <div className="folder">
+                                    <img src={folder} alt="folder" />
+                                </div>
+                                <div className='ps-3'>
+                                    <h4 className="text-aqua text-uppercase mt-4">&lt;<span className="text-light">Proyectos</span>&nbsp; &frasl; &gt;</h4>
+                                    <p className="fw-200">
+                                        En esta sección encontraras mis proyectos personales
+                                    </p>
+                                    <button className="button">Abrir</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="aqua-border">
+                            <div className="box my-4 d-flex">
+                                <div className="folder">
+                                    <img src={folder} alt="folder" />
+                                </div>
+                                <div className='ps-3'>
+                                    <h4 className="text-aqua text-uppercase mt-4">&lt;<span className="text-light">Proyectos en Equipo</span>&nbsp; &frasl; &gt;</h4>
+                                    <p className="fw-200">
+                                        Por último, aqui guardo mis proyectos y colaboraciones hechas con otras personas
+                                    </p>
+                                    <button className="button">Abrir</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
